b3cc514628996e21984647cd3285cd00fb89a9ab
OpatileKelobang <49315650+OpatileKelobang@users.noreply.github.com>
2020-12-27 21:13:56 +0200

    Payload

--- src/main/java/dev/kelobang/userregistration/payload/request/LoginRequest.java
+++ src/main/java/dev/kelobang/userregistration/payload/request/LoginRequest.java
@@ -1,4 +1,20 @@
 package dev.kelobang.userregistration.payload.request;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Getter
+@Setter
 public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
 }
---
+++ src/main/java/dev/kelobang/userregistration/payload/request/SignupRequest.java
@@ -0,0 +1,31 @@
+package dev.kelobang.userregistration.payload.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Data
+@Getter
+@Setter
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 150)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
